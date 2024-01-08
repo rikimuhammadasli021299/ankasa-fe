8505779df17f0774f4e10e313d82ace20642c59e
rikimuhammadasli021299 <rikimuhammadasli@gmail.com>
2024-01-09 01:24:37 +0700

    add page flight detail & payment

--- src/app/flight-details/page.js
+++ src/app/flight-details/page.js
@@ -1,11 +1,239 @@
+'use client';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import { useRef, useState } from 'react';
+import Link from 'next/link';
 
 export default function FlightDetails() {
+  const refLabelFullName = useRef(null);
+  const refLabelEmail = useRef(null);
+  const refLabelPhoneNumber = useRef(null);
+  const refCardPhoneNumber = useRef(null);
+  const refLabelTitle = useRef(null);
+  const refCardTitle = useRef(null);
+  const refLabelNationality = useRef(null);
+  const refCardNationality = useRef(null);
+  const [checked, setChecked] = useState(false);
+  const [toggleSameContact, setToggleSameContact] = useState(false);
+  const [fullname, setFullname] = useState();
+  const [fullnameSameAsContact, setFullnameSameAsContact] = useState();
+
+  const onFocusInputFullName = () => {
+    const labelFullName = refLabelFullName.current;
+    labelFullName.className = 'text-[#000] text-[14px] font-normal';
+  };
+  const onBlurInputFullName = () => {
+    const labelFullName = refLabelFullName.current;
+    labelFullName.className = 'text-[#9B96AB] text-[14px] font-normal';
+  };
+  const onFocusInputEmail = () => {
+    const labelEmail = refLabelEmail.current;
+    labelEmail.className = 'text-[#000] text-[14px] font-normal mt-5';
+  };
+  const onBlurInputEmail = () => {
+    const labelEmail = refLabelEmail.current;
+    labelEmail.className = 'text-[#9B96AB] text-[14px] font-normal mt-5';
+  };
+  const onFocusInputPhoneNumber = () => {
+    const labelPhoneNumber = refLabelPhoneNumber.current;
+    const cardPhoneNumber = refCardPhoneNumber.current;
+    labelPhoneNumber.className = 'text-[#000] text-[14px] font-normal mt-5';
+    cardPhoneNumber.className = 'flex w-full border-b-2 border-b-[#2395ff] pt-3';
+  };
+  const onBlurInputPhoneNumber = () => {
+    const labelPhoneNumber = refLabelPhoneNumber.current;
+    const cardPhoneNumber = refCardPhoneNumber.current;
+    labelPhoneNumber.className = 'text-[#9B96AB] text-[14px] font-normal mt-5';
+    cardPhoneNumber.className = 'flex w-full border-b-2 pt-3';
+  };
+  const onFocusInputTitle = () => {
+    const labelTitle = refLabelTitle.current;
+    const cardTitle = refCardTitle.current;
+    labelTitle.className = 'text-[#000] text-[14px] font-normal mt-5';
+    cardTitle.className = 'flex w-full border-b-2 border-b-[#2395ff] pt-3 mb-5';
+  };
+  const onBlurInputTitle = () => {
+    const labelTitle = refLabelTitle.current;
+    const cardTitle = refCardTitle.current;
+    labelTitle.className = 'text-[#9B96AB] text-[14px] font-normal mt-5';
+    cardTitle.className = 'flex w-full border-b-2 pt-3 mb-5';
+  };
+  const onFocusInputNationality = () => {
+    const labelNationality = refLabelNationality.current;
+    const cardNationality = refCardNationality.current;
+    labelNationality.className = 'text-[#000] text-[14px] font-normal mt-5';
+    cardNationality.className = 'flex w-full border-b-2 border-b-[#2395ff] pt-3 mb-3';
+  };
+  const onBlurInputNationality = () => {
+    const labelNationality = refLabelNationality.current;
+    const cardNationality = refCardNationality.current;
+    labelNationality.className = 'text-[#9B96AB] text-[14px] font-normal mt-5';
+    cardNationality.className = 'flex w-full border-b-2 pt-3 mb-3';
+  };
+  const onCheck = () => {
+    setChecked(!checked);
+  };
+
   return (
     <>
       <Navbar />
-      <h1 className='h-screen pt-20'>FlightDetails</h1>
+      <div className='max-w-[1366px] mx-auto bg-blue mt-[135px] h-[177px] rounded-b-[30px] relative'>
+        <img src='/icon/plane.svg' alt='logo' className='absolute bottom-0' />
+      </div>
+      <div className='max-w-[1366px] mx-auto mt-[-125px]'>
+        <div className='flex w-full flex-col-reverse gap-y-[50px] items-center md:items-start md:flex-row bg-[#F5F6FA] px-4 sm:px-6 lg:px-8'>
+          <div className='flex md:w-[60%] lg:w-[70%] w-[320px] flex-col z-10 md:px-7 px-0'>
+            <h1 className='text-[#000] md:text-white text-[24px] font-semibold'>Contact Person Details</h1>
+            <div className='flex flex-col w-full bg-white rounded-[15px] p-5 mt-6'>
+              <h1 ref={refLabelFullName} className='text-[#9B96AB] text-[14px] font-normal'>
+                Full Name
+              </h1>
+              <input
+                type='text'
+                value={fullname}
+                className='w-full border-b-2 outline-none focus:border-b-[#2395ff] text-[16px] text-[#000] font-normal py-2'
+                onFocus={onFocusInputFullName}
+                onBlur={onBlurInputFullName}
+                onChange={(e) => setFullname(e.target.value)}
+              />
+              <h1 ref={refLabelEmail} className='text-[#9B96AB] text-[14px] font-normal mt-5'>
+                Email
+              </h1>
+              <input type='email' className='w-full border-b-2 outline-none focus:border-b-[#2395ff] text-[16px] text-[#000] font-normal py-2' onFocus={onFocusInputEmail} onBlur={onBlurInputEmail} />
+              <h1 ref={refLabelPhoneNumber} className='text-[#9B96AB] text-[14px] font-normal mt-5'>
+                Phone Number
+              </h1>
+              <div ref={refCardPhoneNumber} className='flex w-full border-b-2 pt-3'>
+                <select className='outline-none focus:outline-none' onFocus={onFocusInputPhoneNumber} onBlur={onBlurInputPhoneNumber}>
+                  <option value='+62'>+62</option>
+                  <option value='+1'>+1</option>
+                  <option value='+33'>+33</option>
+                  <option value='+44'>+44</option>
+                  <option value='+61'>+61</option>
+                  <option value='+81'>+81</option>
+                  <option value='+65'>+65</option>
+                  <option value='+60'>+60</option>
+                </select>
+                <input type='number' className='text-[16px] text-[#000] font-normal w-full px-3 outline-none focus:outline-none' onFocus={onFocusInputPhoneNumber} onBlur={onBlurInputPhoneNumber} />
+              </div>
+              <div className='flex px-5 py-3 mt-9 mb-[18px] rounded-[10px] items-center gap-x-3 bg-[rgba(242,69,69,0.10)]'>
+                <img src='icon/warning.svg' alt='warning' />
+                <h1 className='text-[#595959] text-[14px] font-normal'>Make sure the customer data is correct.</h1>
+              </div>
+            </div>
+            <h1 className='text-[#000] text-[24px] mt-[50px] font-semibold'>Passenger Details</h1>
+            <div className='flex flex-col w-full bg-white rounded-[15px] p-5 mt-6'>
+              <div className='flex flex-wrap px-5 py-3 rounded-[10px] items-center justify-between bg-[rgba(35,149,255,0.10)]'>
+                <h1 className='text-[#595959] text-[14px] font-semibold'>Passenger : 1 Adult</h1>
+                <div className='flex items-center md:gap-x-3 gap-x-1'>
+                  <h1 className='text-[#595959] text-[14px] font-semibold'>Same as contact person</h1>
+                  <label htmlFor='toggle' className={`${toggleSameContact ? 'bg-[#2395ff]' : 'bg-[#C4C4C4]'} w-[55px] h-[30px] rounded-full cursor-pointer relative peer-checked:bg-yellow-300`}>
+                    <input type='checkbox' id='toggle' className='sr-only peer' onChange={() => setToggleSameContact(!toggleSameContact)} />
+                    <span className='bg-white w-[24px] h-[24px] rounded-full absolute left-[3px] top-[3px] peer-checked:left-[28px] transition-all duration-500'></span>
+                  </label>
+                </div>
+              </div>
+              <h1 ref={refLabelTitle} className='text-[#9B96AB] text-[14px] font-normal mt-5'>
+                Title
+              </h1>
+              <div ref={refCardTitle} className='flex w-full border-b-2 pt-3 mb-5'>
+                <select className='outline-none focus:outline-none' onFocus={onFocusInputTitle} onBlur={onBlurInputTitle}>
+                  <option value='Mr.'>Mr.</option>
+                  <option value='Mrs.'>Mrs.</option>
+                </select>
+              </div>
+              <h1 ref={refLabelFullName} className='text-[#9B96AB] text-[14px] font-normal'>
+                Full Name
+              </h1>
+              <input
+                type='text'
+                value={toggleSameContact ? fullname : fullnameSameAsContact}
+                className='w-full border-b-2 outline-none focus:border-b-[#2395ff] text-[16px] text-[#000] font-normal py-2'
+                onFocus={onFocusInputFullName}
+                onBlur={onBlurInputFullName}
+                onChange={(e) => (toggleSameContact ? null : setFullnameSameAsContact(e.target.value))}
+              />
+              <h1 ref={refLabelNationality} className='text-[#9B96AB] text-[14px] font-normal mt-5'>
+                Nationality
+              </h1>
+              <div ref={refCardNationality} className='flex w-full border-b-2 pt-3 mb-3'>
+                <select className='outline-none focus:outline-none' onFocus={onFocusInputNationality} onBlur={onBlurInputNationality}>
+                  <option value='Australia'>Australia</option>
+                  <option value='France'>France</option>
+                  <option value='Indonesia'>Indonesia</option>
+                  <option value='Japan'>Japan</option>
+                  <option value='Malaysia'>Malaysia</option>
+                  <option value='Singapore'>Singapore</option>
+                  <option value='United Kingdom'>United Kingdom</option>
+                  <option value='United States'>United States</option>
+                </select>
+              </div>
+            </div>
+            <h1 className='text-[#000] text-[24px] mt-[50px] font-semibold'>Passenger Details</h1>
+            <div className='flex flex-col w-full bg-white rounded-[15px] py-5 mt-6'>
+              <div className='flex px-6 py-3 items-center justify-between border-b'>
+                <div className='flex items-center gap-x-3'>
+                  <div className={`relative cursor-pointer w-[17px] h-[17px] border-2 border-[#2395ff] ${checked ? 'bg-blue' : null} rounded-[3px]`} onClick={onCheck}>
+                    <img src='/icon/icon-check.svg' alt='check' width={15} height={15} className={`absolute opacity-0 ${checked ? 'opacity-100' : 'opacity-0'} z-50`} onClick={onCheck} />
+                  </div>
+                  <h1 className='text-[#000] text-[18px] font-semibold'>Travel Insurance</h1>
+                </div>
+                <h1 className='text-blue text-[18px] font-bold'>
+                  $ 2.00 <span className='text-[#979797] text-[14px] font-semibold'>/pax</span>
+                </h1>
+              </div>
+              <h1 className='text-[#000] px-6 text-[14px] font-normal mt-5'>Get travel compensation up to $ 10.000,00</h1>
+            </div>
+            <div className='flex mx-auto my-[40px] justify-center w-[278px] bg-blue rounded-[10px] px-3 py-3 shadow-[0px_8px_10px_0px_rgba(35,149,255,0.30)]'>
+              <Link href={'/payment'} className='text-[#FFF] text-[18px] font-bold'>
+                Proceed to Payment
+              </Link>
+            </div>
+          </div>
+          <div className='flex flex-col md:w-[40%] lg:w-[30%] w-[320px] z-10'>
+            <div className='flex w-full items-center justify-between'>
+              <h1 className='text-white text-[24px] font-semibold'>Flight Details</h1>
+              <h1 className='text-white text-[16px] font-semibold'>View Details</h1>
+            </div>
+            <div className='flex flex-col bg-white w-full rounded-[15px] mt-6 py-7'>
+              <div className='flex items-center gap-x-[20px] px-5'>
+                <img src='/image/citilink.png' alt='airlines' className='w-[150px] h-[75px] object-cover' />
+                <h1 className='text-[#595959] text-[16px] font-medium'>Citilink</h1>
+              </div>
+              <div className='flex justify-between mt-[20px] px-5'>
+                <h1 className='text-[#000] text-[18px] font-medium'>Medan (IDN)</h1>
+                <img src='/icon/one-way-dark.svg' alt='logo' />
+                <h1 className='text-[#000] text-[18px] font-medium'>Tokyo (JPN)</h1>
+              </div>
+              <div className='flex gap-x-5 mt-[20px] px-5'>
+                <h1 className='text-[#6B6B6B] text-[12px] font-normal'>Sunday, 15 August 2020</h1>
+                <div className='flex gap-x-2'>
+                  <img src='/icon/ellipse-dark.svg' alt='ellipse' />
+                  <h1 className='text-[#6B6B6B] text-[12px] font-normal'>19:22-21:22</h1>
+                </div>
+              </div>
+              <div className='flex items-center gap-x-2 mt-[20px] px-5'>
+                <img src='/icon/check-list.svg' alt='check' />
+                <h1 className='text-blue text-[14px] font-medium'>Refundable</h1>
+              </div>
+              <div className='flex items-center gap-x-2 mt-[13px] px-5'>
+                <img src='/icon/check-list.svg' alt='check' />
+                <h1 className='text-blue text-[14px] font-medium'>Can reschedule</h1>
+              </div>
+              <div className='flex pt-[20px] border-t mt-[20px]'>
+                <div className='flex w-full justify-between items-center px-5'>
+                  <h1 className='text-[#000] text-[18px] font-medium'>Total Payments</h1>
+                  <div className='flex gap-x-2'>
+                    <h1 className='text-blue md:text-[18px] lg:text-[18px] xl:text-[24px] font-semibold'>$ 145.00</h1>
+                    <img src='/icon/arrow-bottom.svg' alt='arrow' />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <Footer />
     </>
   );
--- src/app/payment/page.js
+++ src/app/payment/page.js
@@ -1,10 +1,193 @@
+'use client';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import { useRef, useState } from 'react';
+
 export default function Payment() {
+  const [creditCardNumber, setCreditCardNumber] = useState();
+  const [expiryDate, setExpiryDate] = useState();
+  const [cvcOrCvv, setCvcOrCvv] = useState();
+  const refCreditCardNumber = useRef(null);
+  const refWrapperCreditCardNumber = useRef(null);
+  const refWrapperExpiryDate = useRef(null);
+  const refWrapperCvcOrCvv = useRef(null);
+  const refInputExpiryDate = useRef(null);
+
+  const onFocusInputCcNumber = () => {
+    const wrapperCcNumber = refWrapperCreditCardNumber.current;
+    wrapperCcNumber.className = 'flex mx-3 p-2 rounded-md border border-solid border-[#2395ff] gap-x-2';
+  };
+  const onBlurInputCcNumber = () => {
+    const wrapperCcNumber = refWrapperCreditCardNumber.current;
+    wrapperCcNumber.className = 'flex mx-3 p-2 rounded-md border border-solid gap-x-2';
+  };
+  const onFocusInputExpiryDate = () => {
+    const wrapperExpiryDate = refWrapperExpiryDate.current;
+    wrapperExpiryDate.className = 'flex p-2 overflow-hidden items-center rounded-md border border-solid border-[#2395ff] gap-x-2';
+  };
+  const onBlurInputExpiryDate = () => {
+    const wrapperExpiryDate = refWrapperExpiryDate.current;
+    wrapperExpiryDate.className = 'flex p-2 overflow-hidden items-center rounded-md border border-solid gap-x-2';
+  };
+  const onFocusInputCvcOrCvv = () => {
+    const wrapperCvcOrCvv = refWrapperCvcOrCvv.current;
+    wrapperCvcOrCvv.className = 'flex p-2 overflow-hidden items-center rounded-md border border-solid border-[#2395ff] gap-x-2';
+  };
+  const onBlurInputCvcOrCvv = () => {
+    const wrapperCvcOrCvv = refWrapperCvcOrCvv.current;
+    wrapperCvcOrCvv.className = 'flex p-2 overflow-hidden items-center rounded-md border border-solid gap-x-2';
+  };
+
+  const handleInputCreditCard = () => {
+    const creditNumber = refCreditCardNumber.current;
+    let num = creditNumber.value;
+
+    let newValue = '';
+    num = num.replace(/\s/g, '');
+    for (let i = 0; i < num.length; i++) {
+      if (i % 4 == 0 && i > 0) newValue = newValue.concat(' ');
+      newValue = newValue.concat(num[i]);
+      creditNumber.value = newValue;
+    }
+  };
+
+  const handleInputExpiryDate = (e) => {
+    const expiryDate = refInputExpiryDate.current;
+    let newInput = expiryDate.value;
+
+    if (e.which !== 8) {
+      let numChars = e.target.value.length;
+      if (numChars == 2) {
+        let thisVal = e.target.value;
+        thisVal += '/';
+        e.target.value = thisVal;
+      }
+    }
+  };
+
   return (
     <>
       <Navbar />
-      <h1 className='h-screen pt-20'>Payment</h1>
+      <div className='max-w-[1366px] flex items-center bg-blue mx-auto mt-[100px]'>
+        <div className='flex bg-white md:flex-row flex-col w-full md:mx-[100px] my-[50px] md:my-[100px] rounded-lg mx-[35px] shadow-xl'>
+          <div className='flex flex-col md:w-[50%] w-full px-3 py-3 md:py-10 md:px-10'>
+            <h1 className='text-[#000] text-[18px] font-semibold'>Payment Method</h1>
+            <div className='flex flex-col bg-slate-200 mt-2 rounded-md py-4 px-3'>
+              <div className='flex justify-between items-center'>
+                <h1 className='text-[#000] font-medium '>Paypal</h1>
+                <img src='/icon/paypal.svg' alt='paypal' className='w-[30px] h-[30px] object-cover' />
+              </div>
+              <div className='flex flex-col md:flex-row justify-between md:items-center items-start mt-[35px]'>
+                <h1 className='text-[#000] font-medium '>Credit Card</h1>
+                <div className='flex flex-wrap gap-x-2'>
+                  <img src='/icon/mastercard.svg' alt='mastercard' className='w-[40px] h-[40px] object-cover' />
+                  <img src='/icon/visa.svg' alt='visa' className='w-[40px] h-[40px] object-cover' />
+                  <img src='/icon/stripe.svg' alt='stripe' className='w-[40px] h-[40px] object-cover' />
+                  <img src='/icon/mastercard.svg' alt='mastercard' className='w-[40px] h-[40px] object-cover' />
+                </div>
+              </div>
+            </div>
+            <h1 className='text-[#313131] text-[13px] font-medium mt-10 ps-3'>Card Number</h1>
+            <div ref={refWrapperCreditCardNumber} className='flex mx-3 p-2 rounded-md border border-solid gap-x-2'>
+              <img src='/icon/credit-card.svg' alt='credit-card' className='w-[30px] h-[30px] object-cover' />
+              <input
+                type='tel'
+                className='text-[#000] text-[13px] w-full outline-none focus:outline-none'
+                ref={refCreditCardNumber}
+                id='number'
+                placeholder='0000 0000 0000 0000'
+                maxLength='19'
+                onKeyUp={handleInputCreditCard}
+                value={creditCardNumber}
+                onFocus={onFocusInputCcNumber}
+                onBlur={onBlurInputCcNumber}
+                onChange={(e) => setCreditCardNumber(e.target.value)}
+              />
+            </div>
+            <div className='flex mt-5 px-3 w-full gap-x-2'>
+              <div className='flex flex-col w-[50%]'>
+                <h1 className='text-[#313131] text-[13px] font-medium'>Expiry Date</h1>
+                <div ref={refWrapperExpiryDate} className='flex p-2 overflow-hidden items-center rounded-md border border-solid gap-x-2'>
+                  <img src='/icon/calendar.svg' alt='calendar' className='w-[20px] h-[20px] object-cover' />
+                  <input
+                    type='text'
+                    className='text-[#000] text-[13px] outline-none focus:outline-none w-full'
+                    maxLength={5}
+                    ref={refInputExpiryDate}
+                    value={expiryDate}
+                    onFocus={onFocusInputExpiryDate}
+                    onBlur={onBlurInputExpiryDate}
+                    onChange={(e) => setExpiryDate(e.target.value)}
+                    onKeyUp={(e) => handleInputExpiryDate(e)}
+                    placeholder='MM/YY'
+                  />
+                </div>
+              </div>
+              <div className='flex flex-col w-[50%]'>
+                <h1 className='text-[#313131] text-[13px] font-medium'>CVC/CVV</h1>
+                <div ref={refWrapperCvcOrCvv} className='flex p-2 overflow-hidden items-center rounded-md border border-solid gap-x-2'>
+                  <img src='/icon/padlock.svg' alt='padlock' className='w-[20px] h-[20px] object-cover' />
+                  <input
+                    type='text'
+                    className='text-[#000] text-[13px] w-full outline-none focus:outline-none'
+                    maxLength={3}
+                    value={cvcOrCvv}
+                    onFocus={onFocusInputCvcOrCvv}
+                    onBlur={onBlurInputCvcOrCvv}
+                    onChange={(e) => setCvcOrCvv(e.target.value)}
+                    placeholder='000'
+                  />
+                </div>
+              </div>
+            </div>
+            <div className='flex items-center gap-x-1 p-3'>
+              <img src='/icon/padlock.svg' alt='padlock' className='w-[12px] h-[12px] object-cover' />
+              <h1 className='text-[10px] text-gray-600 font-normal'>Your transaction is secured with ssl certifacte</h1>
+            </div>
+          </div>
+          <div className='flex flex-col md:w-[50%] w-full px-3 md:ps-0 py-3 md:py-10 md:pr-10'>
+            <h1 className='text-[#000] text-[18px] font-semibold'>Summary</h1>
+            <div className='flex flex-col mt-2 rounded-md py-4 border-b'>
+              <div className='flex flex-wrap justify-between items-center pb-2'>
+                <div className='flex flex-col'>
+                  <div className='flex items-center'>
+                    <h1 className='text-[#000] font-medium'>Pro(Billed Monthly)</h1>
+                    <img src='/icon/triangle.svg' alt='triangle' className='w-[10px] h-[10px] object-cover rotate-180' />
+                  </div>
+                  <h1 className='text-blue text-[10px] font-normal underline'>Save 20% with annual billing</h1>
+                </div>
+                <h1 className='text-[#000] font-medium text-[20px]'>
+                  $9.99<span className='text-secondary text-[8px]'>/month</span>
+                </h1>
+              </div>
+            </div>
+            <div className='flex flex-col mt-3 rounded-md py-4 border-b'>
+              <div className='flex justify-between'>
+                <h1 className='text-[#000] font-medium'>Referral Bonouses</h1>
+                <h1 className='text-[#000] font-medium'>-$2.00</h1>
+              </div>
+              <div className='flex mt-2 justify-between'>
+                <div className='flex items-center gap-x-1'>
+                  <h1 className='text-[#000] font-medium'>VAT</h1>
+                  <img src='/icon/pending.svg' alt='pending' className='w-[15px] h-[15px]' />
+                </div>
+                <h1 className='text-[#000] font-medium'>-20%</h1>
+              </div>
+            </div>
+            <div className='flex flex-col mt-3 rounded-md py-[14px]'>
+              <div className='flex justify-between'>
+                <h1 className='text-[#000] font-medium'>Today you pay(US Dollars)</h1>
+                <h1 className='text-[#000] font-medium'>$0</h1>
+              </div>
+              <h1 className='text-secondary text-[12px] font-normal'>After 30 days $9.59</h1>
+            </div>
+            <div className='flex justify-center bg-blue mt-5 rounded-md py-3'>
+              <h1 className='text-white text-[12px] font-light'>Try it free for 30 days</h1>
+            </div>
+            <h1 className='text-blue underline text-center text-[12px] font-light mt-1'>Have a promo code?</h1>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
